7cbf56c7d4ccaefa0e3e4fb69f3965c9da99f91c
Gheorghiță Mutu <gheorghitamutu@gmail.com>
2023-12-05 18:31:18 +0200

    + add processor function for IIoT data #14

---
+++ project/SensorInputProcessor/SensorInputProcessor/Processor.cs
@@ -0,0 +1,32 @@
+using System;
+using Azure.Messaging.EventHubs;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Logging;
+
+namespace SensorInputProcessor
+{
+    public class Processor
+    {
+        private readonly ILogger<Processor> _logger;
+
+        public Processor(ILogger<Processor> logger)
+        {
+            _logger = logger;
+        }
+
+        [Function(nameof(Processor))]
+        public void Run(
+            [EventHubTrigger(
+                "iothub-ehub-iiot-hub-25385295-39f1a2daba", 
+                Connection = "IOT_HUB_CONNECTION_STRING",
+                ConsumerGroup = "$Default"
+            )] EventData[] events)
+        {
+            foreach (EventData @event in events)
+            {
+                _logger.LogInformation("Event Body: {body}", @event.Body);
+                _logger.LogInformation("Event Content-Type: {contentType}", @event.ContentType);
+            }
+        }
+    }
+}
